1f9d66b6b946495e17354c37cd2c52c524f65837
Devon <devongillis88@gmail.com>
2019-04-09 19:22:49 -0300

    Final Build

--- js/script.js
+++ js/script.js
@@ -12,6 +12,16 @@ function showPage (list, pageNumber) {
 	'use strict';
 	let startIndex = (pageNumber * itemsPerPage) - itemsPerPage;
 	const endIndex = pageNumber * itemsPerPage;
+	
+	for (let x = 0; x < list.length; x++) {
+		for (let t = 0; t < listOfItems.length; t++) {
+			if (list[x] === listOfItems[t]) {
+				listOfItems[t].style.display = 'block';
+			} else {
+				listOfItems[t].style.display = 'none';
+			}
+		}
+	}
 		
 	for (let i = 0; i < list.length; i++) {
 		
@@ -29,10 +39,22 @@ function showPage (list, pageNumber) {
 function appendPagination (list) {
 	
 	'use strict';
-	const pageLinkCount = Math.floor(list.length / itemsPerPage);
 	
-	let html = `<div class="pagination">
-					<ul>`;
+	if (document.querySelector('.pagination')) {
+		document.querySelector('.pagination').remove();
+	}
+	
+	let pageLinkCount = Math.floor(list.length / itemsPerPage);
+	if (pageLinkCount === 0) {
+		pageLinkCount = 1;
+	}
+	
+	const div = document.createElement('div');
+	div.classList.add('pagination');
+	
+	document.querySelector('.page').append(div);
+	
+	let html = `<ul>`;
 	for (let i = 1; i <= pageLinkCount; i++) {
 		if (i === 1) {
 			html += `<li>
@@ -49,8 +71,9 @@ function appendPagination (list) {
 	html += `</ul>
 			</div>`;
 	
-	const page = document.querySelector('.page');
-	page.innerHTML += html;
+	div.innerHTML = html;
+	
+	showPage(list, 1);
 	
 	const paginationLinkList = document.querySelector('.pagination').firstElementChild;
 	
@@ -70,9 +93,75 @@ function appendPagination (list) {
 					pageNumber = pageChecker;
 				}
 			}
-			showPage(listOfItems, pageNumber);
+			showPage(list, pageNumber);
 		}													   
 	});
 	
+}
+
+/*******************
+SEARCH BAR
+*******************/
+let searchInput; 
+let searchMatched = true; //Used to control if a user shows up in the search results
+buildSearchBar();
+function buildSearchBar () {
+	'use strict';
+	const html = `<div class="student-search">
+					<input placeholder="Search for students...">
+					<button>Search</button>
+				</div>`;
+	document.querySelector('.page-header').innerHTML += html;
+	
+	searchInput = document.querySelector('input');
+	searchInput.addEventListener('keyup', search);
+	document.querySelector('button').addEventListener('click', search);
+}
+
+function search () {
+	
+	'use strict';
+	const searchValue = searchInput.value;
+	const nameArray = [];
+	
+	if (searchValue !== '' && searchValue !== null) {
+		
+		const names = document.querySelectorAll('H3');
+
+		for(let i = 0; i < searchValue.length; i++) {
+			for(let x = 0; x < names.length; x++) {
+				if (searchValue[i] === names[x].textContent[i]) {
+					searchMatched = true;
+				} else {
+					searchMatched = false;
+				}
+				if (searchMatched) {
+					nameArray.push(listOfItems[x]);
+				}
+			}
+		}
+		
+		if (nameArray === undefined || nameArray.length === 0) {
+			noResults();
+		} else {
+			appendPagination(nameArray);
+		}
+		
+		
+	} else {
+		appendPagination(listOfItems);
+	}
+}
+
+function noResults () {
+	'use strict';
+	for (let i = 0; i < listOfItems.length; i++) {
+		listOfItems[i].style.display = 'none';
+	}
+	document.querySelector('.pagination').remove();
+	const div = document.createElement('div');
+	div.classList.add('pagination');
+	div.textContent = 'Sorry, no results';
+	document.querySelector('.page').append(div);
 }
 
